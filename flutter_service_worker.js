5cba40e6cfd75a4e286a7c91f186a99d3434619a
justinospina10@gmail.com <justinospina10@gmail.com>
2026-08-30 11:12:08 -0500

    Implementar autenticacion mediante Magic Link de Supabase

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "23dac3d4c4dd6c762d7308911682ee70
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "61220e32eb0ddbf62065b15e533ccf5e",
+"flutter_bootstrap.js": "b674fcafd2815c4f9bbd1fc5269cfe08",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6707bca42496a57a2c2da39cf84f8d38",
 "/": "6707bca42496a57a2c2da39cf84f8d38",
-"main.dart.js": "febec24b8f620862060913183648b048",
+"main.dart.js": "64f6eb683a62e9102786d80c95b40312",
 "manifest.json": "c8e7cc87288af8a9ac1b657328078697",
 "version.json": "0f0e6e5cc8999231490030d777c8b967"};
 // The application shell files that are downloaded before a service worker can
