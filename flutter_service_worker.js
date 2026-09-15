368e941215a2e31fa448ef8758a1708fee83c2b3
justinospina10@gmail.com <justinospina10@gmail.com>
2026-09-15 11:59:59 -0500

    Reconocimiento y verificacion de la foto de perfil

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b42f113e45154aa794af1c266cb3c846
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "23e412da5edc1594623e61c241238708",
+"flutter_bootstrap.js": "c15441d6d0125a912612b9ea548cbd73",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
